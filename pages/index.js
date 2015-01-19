a37e797a19eaf2cc82dbddeb6aaa7a7a4ff5ce6e
Benjamin Tamborine <vtambourine@ya.ru>
2015-01-20 01:29:45 +0300

    Shade line

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,44 @@
-var eurDynamics = JSON.parse(document.getElementById('eur').innerHTML);
-var usdDynamics = JSON.parse(document.getElementById('usd').innerHTML);
+var eurRates = JSON.parse(document.getElementById('eur').innerHTML);
+var usdRates = JSON.parse(document.getElementById('usd').innerHTML);
 
 var path = [];
-var steps = Math.min(eurDynamics.length, usdDynamics.length);
+var steps = Math.min(eurRates.length, usdRates.length);
 
 for (var i = 0; i < steps; i++) {
-    path.push([usdDynamics.data[i].value, eurDynamics.data[i].value]);
+    path.push([usdRates.data[i].value, eurRates.data[i].value]);
 }
 
-var map;
 ymaps.ready(function () {
-    map = new ymaps.Map("ymaps", {
+    var map = new ymaps.Map("ymaps", {
         center: [50, 37],
         zoom: 5,
         controls: ['zoomControl']
     });
 
-    var lineStringGeometry = new ymaps.geometry.LineString(path, {
-        geodesic: true
-    });
+    var segmentColor = 'ff0000';
 
-    var lineStringGeoObject = new ymaps.GeoObject({
-        geometry: lineStringGeometry,
-        properties: {}
-    }, {
-        strokeColor: 'ff0000ff',
-        strokeWidth: 2
-    });
+    var segments = 16;
+    var step = Math.ceil(path.length/segments);
+    for (var i = 0; i < segments; i++) {
+        var segment = path.slice(
+            i * step,
+            (i + 1) * step + 1
+        );
 
-    map.geoObjects.add(lineStringGeoObject);
+        var lineStringGeometry = new ymaps.geometry.LineString(segment, {
+            geodesic: true
+        });
+
+        var lineStringGeoObject = new ymaps.GeoObject({
+            geometry: lineStringGeometry,
+            properties: {}
+        }, {
+            strokeColor: segmentColor + (Math.ceil((255 - 32) / (segments - i)) + 32).toString(16),
+            strokeWidth: 2
+        });
+
+        map.geoObjects.add(lineStringGeoObject);
+    }
 
     map.setBounds(lineStringGeometry.getBounds());
 
@@ -39,10 +49,10 @@ ymaps.ready(function () {
     });
     map.geoObjects.add(placemark);
 
-    var placemark = new ymaps.Placemark(path[0], {
+    /*var placemark = new ymaps.Placemark(path[0], {
         iconContent: 'Год назад'
     }, {
         preset: "islands#blueStretchyIcon"
     });
-    map.geoObjects.add(placemark);
+    map.geoObjects.add(placemark);*/
 });
